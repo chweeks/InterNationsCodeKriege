1ded0cc196fd5aa63bed16dfe6bf3046ab5e6d41
Chris Weeks <chris.weeks@internations.org>
2019-10-23 02:10:49 +0200

    Add test suite

---
+++ test/test.js
@@ -0,0 +1,29 @@
+var assert = require('assert')
+describe('Challenge 1', function() {
+    describe('test case 1', function() {
+        it('', function() {})
+    })
+})
+describe('Challenge 2', function() {
+    describe('test case 1', function() {
+        it('', function() {})
+    })
+})
+
+describe('Challenge 3', function() {
+    describe('test case 1', function() {
+        it('', function() {})
+    })
+})
+
+describe('Challenge 4', function() {
+    describe('test case 1', function() {
+        it('', function() {})
+    })
+})
+
+describe('Challenge 5', function() {
+    describe('test case 1', function() {
+        it('', function() {})
+    })
+})
