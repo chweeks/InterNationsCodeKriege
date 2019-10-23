37d45481bdd16bf6bda2dee319b53134e2045221
Chris Weeks <chris.weeks@internations.org>
2019-10-23 02:22:19 +0200

    test

--- test/test.js
+++ test/test.js
@@ -5,25 +5,25 @@ describe('Challenge 1', function() {
     })
 })
 describe('Challenge 2', function() {
-    describe('test case 1', function() {
+    describe('test case 2', function() {
         it('', function() {})
     })
 })
 
 describe('Challenge 3', function() {
-    describe('test case 1', function() {
+    describe('test case 3', function() {
         it('', function() {})
     })
 })
 
 describe('Challenge 4', function() {
-    describe('test case 1', function() {
+    describe('test case 4', function() {
         it('', function() {})
     })
 })
 
 describe('Challenge 5', function() {
-    describe('test case 1', function() {
+    describe('test case 5', function() {
         it('', function() {})
     })
 })
